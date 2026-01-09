326a35bbb05a32cca0f5314d1a2e757dd52c5016
LuckyLalwani <lalwanilucky333@gmail.com>
2026-01-09 20:52:44 +0530

    implement a hoc for managing window state and add the first window -terminal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import Dock from "./components/Dock"
 import Navbar from "./components/navbar"
 import Welcome from "./components/Welcome"
+import gsap from "gsap"
+
+import { Draggable } from "gsap/Draggable"
+import TerminalWindow from "./windows/terminal"
+gsap.registerPlugin(Draggable);
 
 const App = () => {
   return (
     <main>
       <Navbar/>
       <Welcome/>
+      <Dock/>
+
+      <TerminalWindow/>
     </main>
   )
 }
